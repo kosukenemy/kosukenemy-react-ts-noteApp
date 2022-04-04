5168396d75c22568e6c7d3aae603a403be806f92
kosukenemy <kosukenemy@gmail.com>
2022-04-04 22:47:50 +0900

    update: vite.configにbaseを追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './src',
-  base: './',
-  build: {
-    outDir: '../docs'
-  },
   server: {
     open: true,
   },
-  plugins: [react()],
-  define: {
-    ENV_AUTH_TOKEN: JSON.stringify('246513f2-92d1-11ec-b909-0242ac120002')
-  }
+  base: './',
+  root: './src',
+  build: {
+    outDir: '../docs',
+  },
+  plugins: [react()]
 })
+
